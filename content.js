3658bc2ca7fb7e0890d4c63120adb87ce3463023
joebordes <joe@tsolucio.com>
2021-04-10 16:08:59 +0200

    copy paste clear buttons

--- content.js
+++ content.js
@@ -1,7 +1,26 @@
+function cbGetSelectedText(d) {
+	var S='';
+	if (d.all) {
+		S=d.selection.createRange().text;
+	} else {
+		S=d.getSelection().toString();
+	}
+	return S;
+}
+
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
-		if (request.message === 'clicked_browser_action') {
-			chrome.runtime.sendMessage({'message': 'open_corebos_new_tab'});
+		switch (request.message) {
+			case 'clicked_browser_action':
+				chrome.runtime.sendMessage({'message': 'open_corebos_new_tab'});
+				break;
+			case 'getSelectedText':
+				chrome.runtime.sendMessage({
+					'message': 'selectedtext',
+					'selectedtext': cbGetSelectedText(document),
+					'payload': request.payload
+				});
+				break;
 		}
 	}
 );
--- popup.js
+++ popup.js
@@ -1,8 +1,43 @@
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		switch (request.message) {
+			case 'selectedtext':
+				if (request.payload.field=='description') {
+					document.getElementById('description').value = request.selectedtext;
+				} else {
+					document.getElementById('title').value = request.selectedtext;
+				}
+				//chrome.tabs.create({"url": request.url});
+				break;
+		}
+	}
+);
+
 document.getElementById('sendtocb').onclick=function (e) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		var activeTab = tabs[0];
 		chrome.tabs.sendMessage(activeTab.id, {'message': 'clicked_browser_action'});
 	});
+};
+
+document.getElementById('titlepaste').onclick=function (e) {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {'message': 'getSelectedText', 'payload':{'field':'title'}});
+	});
+};
+
+document.getElementById('titleclear').onclick=function (e) {
+	document.getElementById('title').value = '';
+};
+
+document.getElementById('descpaste').onclick=function (e) {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {'message': 'getSelectedText', 'payload':{'field':'description'}});
+	});
+};
+
+document.getElementById('descclear').onclick=function (e) {
+	document.getElementById('description').value = '';
 };
 
 document.getElementById('convertto').onchange=function (e) {
