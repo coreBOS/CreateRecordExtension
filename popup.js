6fda4559c3286ccb353eb9bd53959fdde76ba31a
jenadius.nicholaus <jenadius.nicholaus@axela.io>
2021-05-26 22:40:31 +0300

    feat(Popup) persist the custom field values on Popup open

--- popup.js
+++ popup.js
@@ -82,13 +82,13 @@ document.getElementById('convertto').onchange=function (e) {
 			let fieldnames = {};
 			allFields.forEach((textfield) => textfield.addEventListener('change', (e) => {
 				fieldnames[e.target.name] = e.target.value;
-				chrome.storage.sync.set({"fieldnames":fieldnames});	
+				chrome.storage.sync.set({'fieldnames':fieldnames});
 			}));
 			chrome.storage.sync.get('fieldnames', ({fieldnames}) => {
-				Object.keys(fieldnames).map(function(key) {
+				Object.keys(fieldnames).map( function (key) {
 					if (typeof fieldnames[key] != 'undefined') {
 						document.getElementById(key).value = fieldnames[key];
-					}		
+					}
 				});
 			});
 		}
